387486bba9f629a3c6b4e27f97b46469598177ae
Jake Salmon <salmon.jake16@gmail.com>
2017-07-11 00:31:40 +0100

    Modified map fragment. Modified map.java file. Modified the Android manifest file by including permissions for location data.

--- app/src/main/java/com/example/android/universityproject/Maps.java
+++ app/src/main/java/com/example/android/universityproject/Maps.java
@@ -1,108 +1,97 @@
 package com.example.android.universityproject;
 
+import android.Manifest;
 import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Maps.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Maps#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Maps extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class Maps extends FragmentActivity implements OnMapReadyCallback,
+        ActivityCompat.OnRequestPermissionsResultCallback {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    // Declare private global instance variable mMap of class GoogleMap
+    private GoogleMap mMap;
 
-    public Maps() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Maps.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Maps newInstance(String param1, String param2) {
-        Maps fragment = new Maps();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    // Id to identify a location permission request.
+    private static final int REQUEST_LOCATION = 1;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        setContentView(R.layout.fragment_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    // Method that creates a map and places a pin over london.
+    // Calls the request location method.
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in London, England, and move the camera.
+        LatLng london = new LatLng(51.5074, 0.1278);
+        mMap.addMarker(new MarkerOptions().position(london).title("Marker in London"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(london));
+
+        requestLocationPermission();
+
+        if(gpsCheck() == false){
+            locDiaFragSwitch();
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+
+
+    // This method starts intent to open activity advising that location services have not been enabled.
+    public void locDiaFragSwitch(){
+        //Intent locIntent = new Intent(this, LocDialogFragment.class);
+        //startActivity(locIntent);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    //This method checks to see whether location services is enabled.
+    // It returns a true if enabled, or false if not.
+    public  boolean gpsCheck(){
+        LocationManager location = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        if(location.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+
+            return true;
+        }else{
+            return false;
         }
     }
 
+    // This method requests permission from the user to allow access to location services.
+    private void requestLocationPermission(){
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                REQUEST_LOCATION);
+    }
+
+
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        mMap.setMyLocationEnabled(true);
+                    }
+                }
+            }
         }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
