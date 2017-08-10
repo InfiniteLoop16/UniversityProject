ccf176dcc5a5ca979ff00896214db127041b9145
Jake Salmon <salmon.jake16@gmail.com>
2017-08-10 13:17:36 +0100

    Added NewPost activity and Layout file.

---
+++ app/src/main/java/com/example/android/universityproject/NewPost.java
@@ -0,0 +1,86 @@
+package com.example.android.universityproject;
+
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class NewPost extends AppCompatActivity {
+
+    private ListItem newPost;
+    private EditText eTitle;
+    private EditText eBody;
+    private DatabaseReference mFirebaseDatabaseReference;
+    private DatabaseReference mDatabaseChat;
+    private DatabaseReference mDatabaseConvo;
+    private FirebaseUser mUser;
+    private FirebaseAuth mFirebaseAuth;
+    private String keyId;
+    private String mTimeAndDate;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_post);
+
+        FloatingActionButton sendPost = (FloatingActionButton) findViewById(R.id.Send_Post);
+        sendPost.setOnClickListener(mSendPost);
+
+        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mDatabaseChat = mFirebaseDatabaseReference.child("chat");
+        mDatabaseConvo = mFirebaseDatabaseReference.child("Convo's");
+
+        eTitle = (EditText)findViewById(R.id.TitleBox);
+        eBody = (EditText)findViewById(R.id.BodyBox);
+
+        newPost = new ListItem();
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mUser = mFirebaseAuth.getCurrentUser();
+
+
+
+
+
+    }
+
+    public View.OnClickListener mSendPost = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String title = eTitle.getText().toString();
+            String body = eBody.getText().toString();
+
+
+            newPost.setTitle(title);
+            newPost.setBody(body);
+            mTimeAndDate = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss a").format(new Date());
+            newPost.setTimeAndDateSent(mTimeAndDate);
+            newPost.setUserName(mUser.getDisplayName());
+            newPost.setUniqueKey(mTimeAndDate+" "+ newPost.getUserName());
+
+
+
+            DatabaseReference myRef = mDatabaseChat.push();
+            keyId = myRef.getKey();
+            newPost.setId(keyId);
+            myRef.setValue(newPost);
+            mDatabaseConvo.child(keyId).push().setValue(newPost);
+
+            finish();
+        }
+    };
+
+
+}
+
