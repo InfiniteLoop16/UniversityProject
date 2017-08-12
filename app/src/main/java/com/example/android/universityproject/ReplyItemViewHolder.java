656b9b305a5cce8c8a0877e7b8bc3f6713c485a6
Jake Salmon <salmon.jake16@gmail.com>
2017-08-12 13:52:09 +0100

    Added reply recycler activity and connected reply post class and itemview holder.

---
+++ app/src/main/java/com/example/android/universityproject/ReplyItemViewHolder.java
@@ -0,0 +1,59 @@
+package com.example.android.universityproject;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by Jake on 12/08/2017.
+ */
+
+public class ReplyItemViewHolder extends RecyclerView.ViewHolder{
+    private final TextView mTitleFiled;
+    private final TextView mBodyField;
+    private final TextView mUserNameField;
+    private final TextView mTimeStampField;
+    private DatabaseReference mFirebaseDatabase;
+
+
+
+    public ReplyItemViewHolder(View itemView){
+        super(itemView);
+        mTitleFiled = (TextView) itemView.findViewById(R.id.textViewReplyTitle);
+        mBodyField = (TextView) itemView.findViewById(R.id.textViewReplyBody);
+        mUserNameField = (TextView) itemView.findViewById(R.id.userNameReplyView);
+        mTimeStampField = (TextView) itemView.findViewById(R.id.textViewReplyTime);
+
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance().getReference();
+    }
+
+    public void bind(ListItem post){
+        setTitle(post.getTitle());
+        setBody(post.getBody());
+        setUserName(post.getUserName());
+        setTimeAndDate(post.getTimeAndDateSent());
+
+    }
+
+
+    private void setTitle(String title){
+        mTitleFiled.setText(title);
+    }
+
+    private void setBody(String body){
+        mBodyField.setText(body);
+    }
+
+    private void setUserName(String uName){mUserNameField.setText(uName);}
+
+    private void setTimeAndDate(String mCurrentTime){
+        mTimeStampField.setText(mCurrentTime);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/android/universityproject/ReplyRecycler.java
@@ -0,0 +1,101 @@
+package com.example.android.universityproject;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+public class ReplyRecycler extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private DatabaseReference mDataRef;
+    private DatabaseReference mDataRefChild;
+    private String searcher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reply_recycler);
+
+        // Gets the putExtra from the CardView. This is the Unique push ID which forms the node key.
+        Intent i = getIntent();
+        searcher = i.getStringExtra("uniqueID");
+
+        mDataRef = FirebaseDatabase.getInstance().getReference();
+        mDataRefChild = mDataRef.child("Convo's").child(searcher);
+
+
+        // Create RecyclerView
+        mLayoutManager = new LinearLayoutManager(this);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_reply_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        // Creation of FAB onClick Listener for reply post
+        FloatingActionButton addPost = (FloatingActionButton) findViewById(R.id.add_Reply_Post);
+        addPost.setOnClickListener(mAddReplyPostListener);
+
+        // Creation of FAB onClick Listener to share location
+        FloatingActionButton shareLocation = (FloatingActionButton) findViewById(R.id.share_Location);
+        shareLocation.setOnClickListener(mShareLocation);
+
+
+
+
+        // FirebaseRecycler Adapter Subclass.
+        mAdapter = new FirebaseRecyclerAdapter<ListItem, ReplyItemViewHolder>(
+                ListItem.class,
+                R.layout.reply_recycler_item,
+                ReplyItemViewHolder.class,
+                mDataRefChild) {
+
+            @Override
+            protected void populateViewHolder(ReplyItemViewHolder viewHolder, ListItem post, int position) {
+                viewHolder.bind(post);
+
+            }
+        };
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private View.OnClickListener mAddReplyPostListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(v.getContext(), ReplyPost.class);
+            i.putExtra("ChildNode", searcher);
+            startActivity(i);
+
+        }
+    };
+
+    private View.OnClickListener mShareLocation = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(v.getContext(), Maps.class);
+            i.putExtra("ChildNode", searcher);
+            startActivity(i);
+        }
+    };
+
+
+}
+
+
